7a090b52d33d26fdc60e5fa6a4c735db4fe6e1db
Ielmahi <vodkailias@gmail.com>
2024-01-16 13:49:56 +0100

    update

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-start = 97
-
-while start <= 122:
-    print("{}".format(chr(start)), end='')
-    start += 1
+for character in range(97, 123):
+    print("{:c}".format(character), end='')

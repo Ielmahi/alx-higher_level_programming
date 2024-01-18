46ffb88b9b3a4e854c157aee5e74a742492085d8
Ielmahi <vodkailias@gmail.com>
2024-01-18 05:03:51 -0800

    update

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    lenght = len(sys.argv) -1
+    if lenght == 0:
+        print("{} arguments.".format(lenght))
+    elif lenght == 1:
+        print("{} argument:".format(lenght))
+    else:
+        print("{} arguments:".format(lenght))
+    for i in range(1,lenght):
+        print("{}: {}".format(i, sys.argv[i]))

23e2e4c71a764a6ab9e52a2a320417536f2bcf2f
Ielmahi <vodkailias@gmail.com>
2024-01-18 03:38:19 -0800

    update

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
-#!/usr/bin/env python3
-import add_0
-a = 1
-b = 2
-add_0.add(a,b)
-
-=======
 #!/usr/bin/python3
 if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
->>>>>>> 6961a6229e28eebf125f2420162752bdd6309cf5
